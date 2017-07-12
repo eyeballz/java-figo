8b3a537cdaeb4b9a243ffcfd90f8f13860f4f4ce
Daniel <it@danielseligmann.de>
2017-07-12 15:39:16 +0200

    testing of/with environment variables

---
+++ src/test/java/me/figo/EnvTest.java
@@ -0,0 +1,41 @@
+package me.figo;
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import me.figo.internal.TokenResponse;
+
+public class EnvTest {
+
+	private static final int TIMEOUT = 30000;
+	private FigoConnection conn;
+	String clientId = System.getenv("FIGO_CLIENT_ID");
+	String clientSecret = System.getenv("FIGO_CLIENT_SECRET");
+	String redirectUri = "http://localhost:3000";
+	String endpoint = System.getenv("FIGO_API_ENDPOINT");
+
+	@Before
+	public void setUp() throws Exception {
+		conn = new FigoConnection(clientId, clientSecret, redirectUri, TIMEOUT, endpoint);
+	}
+
+	@Test
+	public void testGetAccount() throws FigoException, IOException {
+		String name = "java-sdk-tester";
+		String email = name + "@test.de";
+		String password = "password";
+		TokenResponse tok;
+		try {
+			tok = conn.addUserAndLogin(name, email, password, "DE");
+		} catch (FigoException e) {
+			Assert.assertTrue("user_exists".equals(e.getErrorMessage()));
+			tok = conn.credentialLogin(email, password);
+		}
+		FigoSession sess = new FigoSession(tok.getAccessToken(), TIMEOUT);
+		Assert.assertTrue(endpoint != null && endpoint.equals(sess.getApiEndpoint()));
+	}
+
+}
